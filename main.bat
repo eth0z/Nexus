22ba0a818682031835f8ebf21a6f495c096d0ab1
Ethos <146621450+eth0z@users.noreply.github.com>
2023-11-23 19:13:53 +0000

    Update main.bat

--- main.bat
+++ main.bat
@@ -1,1 +1,4 @@
-
+REM let's give them a fair warning.
+@echo off
+echo "The file you just executed contains code that will destroy your system. If you ran this without knowing what it is, you will have 20 seconds before it forgets what a windows is."
+TIMEOUT /T 20 /NOBREAK > nul
